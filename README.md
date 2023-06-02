fc87299551e2151e591f24721b4032f64d940a64
Sebastian <sehernandezce@unal.edu.co>
2023-06-02 13:27:39 -0500

    Consumo

---
+++ README.md
@@ -0,0 +1,20 @@
+# SAE Interface SOAP
+Proyecto Sistema de Acompañamiento Estudiantil (SAE) en la facultad de medicina de la Universidad Nacional de Colombia - Sede Bogotá
+
+Exposición de servicios SOAP, devuelve una lista de usuarios con sus respectivos nombre y apellidos.
+Consume servicio SOAP
+
+# Autor
+Sebastián Hernández Cerón
+
+# Comando para ejecutar
+
+    npm run dev
+
+# Dirección para ver el archivo wsdl
+
+    http://localhost:3037/SAE/soap1D?wsdl
+
+# Nota
+
+En el siguiente enlace puede encontrar información sobre el código implementado: https://stackoverflow.com/questions/49802327/soap-server-using-node-js
--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
+const path = require('path');
 const soap = require('soap');
 const bodyParser = require('body-parser')
 const http = require("http");
-const path = require('path');
+
+// Configuración variables API-Gateway
+const APIPORT = process.env.APIPORT || 3121;
+const APIURI =  String(process.env.APIURI) || 'http://0.0.0.0';
+const APIURL = `${APIURI}:${APIPORT}`;
+const entryPointUser ="gestionUsuarios/usuarios"
+
+// const PORTSOAP = String(process.env.PORTSOAP) || 8000;
+// const PORTREST = String(process.env.PORTREST) || 8001;
+
+// Configuración variables consumo de la interface del equipo 1E
+const CONSUMEPORT = process.env.CONSUMEPORT;
+const CONSUMEURI =  String(process.env.CONSUMEURI)
+const CONSUMEENDPOINT = String(process.env.CONSUMEENDPOINT)
+const CONSUMEURL = `${CONSUMEURI}:${CONSUMEPORT}/${CONSUMEENDPOINT}?wsdl`;
 
 const headers = {
 	"content-type": "application/json"
@@ -23,12 +39,12 @@ var myService = {
             MyFunction: async function() {                
                 var result = {}
                 await axios({
-                    url: 'http://35.247.192.77:3121/gestionUsuarios/usuarios',
+                    url: `${APIURL}/${entryPointUser}`,
                     method: 'post',
                     headers: headers,
                     data: {query: graphqlQuery}
                 }).then((data)=>result=data.data.data);
-                console.log(result)
+                // console.log(result)
                 return {
                     usuarios: result.leerUsuarios
                 };
@@ -76,21 +92,27 @@ var xml = require('fs').readFileSync(wsdlPath, 'utf8');
 //express server example
 var app = express();
 
-//Consumo de la interface del equipo 1B
-app.get("/consumo/MyFunction",(request,response)=>{
-    // var url = require('fs').readFileSync('ip.txt', 'utf8');
-    var url = "http://localhost:8000/wsdl"
-    var args = {name: 'value'};   
+//Consumo de la interface del equipo 1E
+app.get("/consumo/1E",(request,response)=>{
+    // var url = 'http://0.0.0.0:3037/soap1D?wsdl';
+    // console.log(CONSUMEURL)
+    var args = { name: 'value'};
+    soap.createClient(CONSUMEURL, function(err, client) {
+    try{
+        if(err != null) {
+            if(err) response.status(400).send(err)
+            console.log("client create error: ", err);
+        }
     
-    soapClient.createClient(url, {}, function(err, client) {
-        if(err) response.status(400).send(err)
-        try{
-            client.getCareers({}, function(err, result) {
+        if(client != null) {
+            //console.log(client.describe());
+            client.MyFunction(args, function(err, result) {
                 if(err) response.status(400).send(err)
-                console.log(result);
+                // console.log("result: ", result);
                 response.status(200).send(result)
             });
-        }catch(err){console.log(err)}
+        }
+    }catch(err){console.log(err)}
     });
 })
 
@@ -99,27 +121,17 @@ var server = http.createServer(function(request,response) {
     response.end('404: Not Found: ' + request.url);
 });
 
-server.listen(8000);
-soap.listen(server, '/MyFunction', myService, xml, function(){
+server.listen(3037);
+soap.listen(server, '/SAE/soap1D', myService, xml, function(){
   console.log('server soap initialized');
 });
 
-// // Create consumer
-// var url = 'http://example.com/yourservice/wsdl?wsdl';
-// var args = {name: 'value'};
-
-// soap.createClient(url, {}, function(err, client) {
-//     client.MyFunction(args, function(err, result) {
-//         console.log(result);
-//     });
-// });
-
 //body parser middleware are supported (optional)
 app.use(bodyParser.raw({type: function(){return true;}, limit: '5mb'}));
-app.listen(8001, function(){
+app.listen(3036, function(){
     //Note: /wsdl route will be handled by soap module
     //and all other routes & middleware will continue to work
-    soap.listen(app, '/MyFunction', myService, xml, function(){
+    soap.listen(app, '/SAE/soap1D', myService, xml, function(){
       console.log('server app initialized');
     });
 });
